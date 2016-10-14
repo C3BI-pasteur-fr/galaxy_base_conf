756aee0bbbba1605e4fd8873c71947049a4c237b
fmareuil <fmareuil@pasteur.fr>
2016-10-14 13:07:55 +0200

    last crons scripts

--- scripts/crons/lib_cron.py
+++ scripts/crons/lib_cron.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 """
 Created on Mars. 04, 2016
 
@@ -7,18 +8,132 @@ Created on Mars. 04, 2016
 @githuborganization: C3BI
 Cron library
 """
+from bioblend import galaxy
+from bioblend.galaxyclient import ConnectionError
 from email.mime.text import MIMEText
 import os
 import smtplib
+import sys
+import ldap3
 
 # Mail information
 GALAXY_EMAIL = "no-reply.galaxy@pasteur.fr"
 ADMINS_EMAILS = "galaxy@pasteur.fr"
+# ADMINS_EMAILS = "fmareuil@pasteur.fr"
 # LDAP information
-BASE_DN = 'ou=personnes,ou=utilisateurs,dc=pasteur,dc=fr'
-ATTRS = ['gidNumber', 'uidNumber']
+BASE_DN = 'dc=corp,dc=pasteur,dc=fr'
+ATTRS = ['proxyAddresses']
 # List of galaxy user exceptions, old production user and duplicate user
 EXCEPTIONS = ['(null)', 'galaxyhimself@pasteur.fr', 'bioweb-prod@pasteur.fr']
+
+welcome_message = """Hello,
+Your data library within Galaxy has been activated and administrated, you can start using it.
+You can copy data in the directory /pasteur/projets/common/galaxy/links/'yourLogin'@pasteur.fr in order to upload them in the galaxy instance.
+An output directory (/pasteur/projets/common/galaxy/outputs/'yourLogin'@pasteur.fr)" was also created for the tool export_data in the tab Send Data.
+This message is transmitted automatically by Galaxy, please do not reply to this email. If you have any questions, please contact informatique@pasteur.fr.
+Don't forget to activate your account.
+
+Best regards,
+The Galaxy team.
+--
+Centre d'Informatique pour la Biologie
+
+
+Bonjour,
+Votre 'librairie' dans galaxy a été paramétrée, vous pouvez maintenant l'utiliser. Vous pouvez déposer vos données dans pasteur /projets/common/galaxy/links/'yourLogin'@pasteur.fr et les importer dans votre 'librairie' via l'interface galaxy.
+Un répertoire de sortie (/pasteur/projets/common/galaxy/outputs/'yourLogin'@pasteur.fr) a aussi été créé pour l'outil export_data dans l'onglet Send Data.
+Ce message vous est transmis automatiquement par Galaxy, merci de ne pas y répondre. Pour toute question, veuillez contacter informatique@pasteur.fr.
+N'oubliez pas d'activer votre compte.
+
+Cordialement,
+L'équipe Galaxy.
+--
+Centre d'Informatique pour la Biologie
+"""
+
+
+def check_ldap_status(email, ldap, ldap_user, ldap_password):
+    """
+    Create ldap connection object check email and return resp
+    :param email: string user email
+    :param ldap: ldap url
+    :param ldap_user: ldap user
+    :param ldap_password: ldap password
+    :return: list
+    """
+    server = ldap3.Server(ldap, use_ssl=True, get_info=ldap3.ALL)
+    with ldap3.Connection(server, auto_bind=True, check_names=True, user=ldap_user, password=ldap_password) as conn:
+        ldap_filter = '(&(proxyAddresses=smtp:{0})' \
+                      '(objectClass=user)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))'.format(email)
+        resp = conn.search(BASE_DN, ldap_filter, attributes=ATTRS)
+        emaillist = []
+        if resp:
+            for mail in conn.response[0]['attributes']['proxyAddresses']:
+                if 'smtp:' in mail.lower():
+                    emaillist.append(mail.strip('smtp:').strip('SMTP:'))
+        return emaillist
+
+
+def check_otheraccount(email, emaillist, users):
+    """
+    Check if a user already have an email/account with pasteur privilege
+    :param email: string email current user
+    :param emaillist: list emails associated with the user
+    :param users: list of galaxy users
+    :return: boolean
+    """
+    for mail in emaillist:
+        if email != mail:
+            for user in users:
+                if user["group_name"] == "pasteur_users" and mail == user["email"]:
+                    return True
+    return False
+
+
+def get_users_and_group_users(gi):
+    """
+    Create dictionnary with user id, user email and user group
+    :param gi: galaxy instance object
+    :return: dictionnary user, pasteur group id, external group id
+    """
+    try:
+        users = gi.users.get_users(deleted=False)
+        users = [{'email': user['email'], 'id': user['id']} for user in users]
+        pasteur_group_id = groupid_from_name(gi, "pasteur_users")
+        pasteur_users = gi.groups.get_group_users(pasteur_group_id)
+        pasteur_users = [{'email': user['email'], 'id': user['id']} for user in pasteur_users]
+        external_group_id = groupid_from_name(gi, "external_users")
+        external_users = gi.groups.get_group_users(external_group_id)
+        external_users = [{'email': user['email'], 'id': user['id']} for user in external_users]
+        for user in users:
+            if user in pasteur_users:
+                user['group_id'] = pasteur_group_id
+                user['group_name'] = "pasteur_users"
+            elif user in external_users:
+                user['group_id'] = external_group_id
+                user['group_name'] = "external_users"
+            else:
+                user['group_id'] = ""
+                user['group_name'] = ""
+
+    except ConnectionError, e:
+        sendmail(ADMINS_EMAILS, "##### ERROR ##### during the display bioblend galaxy command, connection error : {0}."
+                                " CRON: {1}\n".format(e, os.path.basename(__file__)), "Galaxy: user is not in the LDAP")
+        sys.exit(1)
+    return users, pasteur_group_id, external_group_id
+
+
+def groupid_from_name(gi, name):
+    """
+    Return galaxy group id from galaxy group name
+    :param gi: Galaxy instance object
+    :param name: string group name
+    :return: string group id
+    """
+    groups = gi.groups.get_groups()
+    for group in groups:
+        if group['name'] == name:
+            return group['id']
 
 
 def sendmail(recipient, msg, subject):
@@ -40,28 +155,24 @@ def sendmail(recipient, msg, subject):
         s.quit()
 
 
-def link_permissions(path, user, connection, gid):
+def link_permissions(path, user):
     """
-    Change the owner and group id of links folder to the numeric uid and gid of the galaxy user
+    Create link folder
     :param path: links directory path
     :param user: user dictionnary
-    :param connection: ldap connection
-    :param gid: bioweb group gid
     :return: error messages
     """
     msg = str()
     try:
-        folder_links = os.listdir(path)
+        folders_links = os.listdir(path)
     except OSError, e:
         msg += "##### ERROR ##### {0}, {1} path doesn't exist. CRON: {2}\n".format(e, path, os.path.basename(__file__))
         return msg
-    if user['email'] not in folder_links:
+    if user['email'] not in folders_links:
         path_links = os.path.join(path, user['email'])
         try:
-            os.mkdir(path_links, 02750)
-            user_uid = connection.response[0]['attributes']['uidNumber']
-            os.chown(path_links, user_uid, gid)
-            msg += "This user has links folder and permissions ; user email : {email} - user uid: {id}\n".format(**user)
+            os.mkdir(path_links)
+            msg += "This user has links folder ; user email : {email} - user uid: {id}\n".format(**user)
         except OSError, e:
             msg += "##### ERROR ##### during the creation of the links directory of {0[id]} user id, email - " \
                    "{0[email]}.\n {1} \n CRON: {2}\n".format(user, e, os.path.basename(__file__))
@@ -70,53 +181,49 @@ def link_permissions(path, user, connection, gid):
     return msg
 
 
-def output_permissions(path, user, connection, gid):
+def set_library_folder(gi, user, message, base_link):
     """
-    Change the owner and group id of outputs folder to the numeric uid and gid of the galaxy user
-    :param path: outputs directory path
-    :param user: user dictionnary
-    :param connection: ldap connection
-    :param gid: bioweb group gid
-    :return: error messages
+    Create Galaxy library and set permission and create link folder
+    :param gi: Galaxy instance object
+    :param user: dict user
+    :param message: string message for mail
+    :param base_link: path directory link
+    :return: message string for mail
     """
-    msg = str()
-    try:
-        folder_outputs = os.listdir(path)
-    except OSError, e:
-        msg += "##### ERROR ##### {0}, {1} path doesn't exist. CRON: {2}\n".format(e, path, os.path.basename(__file__))
-        return msg
-    if user['email'] not in folder_outputs:
-        path_outputs = os.path.join(path, user['email'])
-        try:
-            os.umask(0)
-            os.mkdir(path_outputs, 02770)
-            user_uid = connection.response[0]['attributes']['uidNumber']
-            os.chown(path_outputs, user_uid, gid)
-            msg += "This user has outputs folder and permissions ; user email :{0} - user uid: {1}\n"\
-                .format(user['email'], user['id'])
-        except OSError, e:
-            msg += "##### ERROR ##### during the creation of the outputs directory of {0[email]} user.\n {1} \n " \
-                   "CRON: {2}\n".format(user, e, os.path.basename(__file__))
+    if gi.libraries.get_libraries(name=user['email'], deleted=False):
+        message += "##### ERROR ##### The library: {0[email]} - library id: {0[id]} already exist " \
+                   "and is not tagged deleted. CRON {1}\n".format(user, os.path.basename(__file__))
+        message += link_permissions(base_link, user)
+    elif not gi.libraries.get_libraries(name=user['email'], deleted=True):
+        roles = gi.roles.get_roles()
+        library_info = gi.libraries.create_library(user['email'],
+                                                   description="Library of {0}".format(user['email']))
+        message += "Creation of the library of {1[email]} - user id: {1[id]} - " \
+                   "library id: {0[id]}\n".format(library_info, user)
+        message += set_library_permissions(gi, library_info, roles, user)
+        message += link_permissions(base_link, user)
+        sendmail(user['email'], welcome_message, "Welcome to (the) Galaxy")
     else:
-        msg += "The user {0[email]} already own a directory 'outputs'\n".format(user)
-    return msg
+        message += "##### ERROR ##### The library: {0[email]} - library id: {0[id]} already exist " \
+                   "but is tagged deleted. CRON {1}\n".format(user, os.path.basename(__file__))
+        message += link_permissions(base_link, user)
+    return message
 
 
-def library_user_check(libraries_list, user):
+def delete_big_mem_quota_user(gi, user):
     """
-    Search a library when the library name == user email
-    :param libraries_list: list of galaxy libraries
-    :param user: information dict of a user
-    :return: library dict
+    Delete user from bi_user quota
+    :param gi: galaxy instance object
+    :param user:  information dict of a user
+    :return:
     """
-    user_library = dict()
-    for lib in libraries_list:
-        if lib['name'] == user['email']:
-            user_library = lib
-            # stop if find a library not deleted
-            if not user_library['deleted']:
-                break
-    return user_library
+    biguser_quota = get_quota_pasteur(gi)
+    info_biguser_quota = gi.quotas.show_quota(biguser_quota['id'])
+    users_biguser_quota = [i['user']['id'] for i in info_biguser_quota['users']]
+    if user['id'] in users_biguser_quota:
+        users_biguser_quota.remove(user['id'])
+        gi.make_put_request(os.path.join(gi.quotas.url, str(biguser_quota['id'])),
+                            {'in_users': users_biguser_quota})
 
 
 def set_library_permissions(galaxy_instance, lib, roles, user):
@@ -126,7 +233,7 @@ def set_library_permissions(galaxy_instance, lib, roles, user):
     :param lib: library dictionnary
     :param roles: list of galaxy roles
     :param user: user dictionnary
-    :return: None
+    :return: message string for mail
     """
     msg = str()
     user_role = dict()
@@ -154,3 +261,17 @@ def set_library_permissions(galaxy_instance, lib, roles, user):
         msg += "##### ERROR ##### the Galaxy user (email {email} - id {id}) has no role. CRON {0}\n"\
             .format(os.path.basename(__file__), **user)
     return msg
+
+
+def get_quota_pasteur(galaxyinstance):
+    """
+    Return the Pasteur dictionnary quota
+    :param galaxyinstance: object GalaxyInstance
+    :return: quota dictionnary
+    """
+    quotas = galaxyinstance.quotas.get_quotas()
+    quota_pasteur = dict()
+    for quota in quotas:
+        if quota['name'] == 'big_user':
+            quota_pasteur = quota
+    return quota_pasteur
--- scripts/crons/remove_inactive_users.py
+++ scripts/crons/remove_inactive_users.py
@@ -1,7 +1,7 @@
 """
 Created on Feb. 19, 2016
 
-@authors: Fabien Mareuil, Institut Pasteur, Paris
+@authors: Fabien Mareuil, Olivia Doppelt-Azeroual, Institut Pasteur, Paris
 @contacts: fabien.mareuil@pasteur.fr
 @project: galaxy_prod_conf
 @githuborganization: C3BI
@@ -13,6 +13,7 @@ from lib_cron import sendmail
 from sqlalchemy import create_engine, select, MetaData
 from sqlalchemy.ext.automap import automap_base
 from lib_cron import ADMINS_EMAILS
+
 
 def map_database(connection):
     """
@@ -28,7 +29,21 @@ def map_database(connection):
     return base.classes, eng
 
 
-def list_inactive_users(datab, eng):
+def period_check(now, timeperiod):
+    """
+    :param now:
+    :param timeperiod:
+    :return: datetime object
+    """
+    mod = (int(timeperiod)%12)
+    div = (int(timeperiod)/12)
+    if int(timeperiod) < int(now.month):
+        return datetime.date(now.year, int(now.month) - mod, now.day)
+    else:
+        return datetime.date(now.year - div, now.month - mod, now.day)
+
+
+def list_inactive_users(datab, eng, timeperiod):
     """
     Send a mail with informations about Galaxy, Total users, numbers of actives users last year, numbers of inactives
     users last year, number of not Pasteur users and inactive and the list of these users
@@ -36,40 +51,55 @@ def list_inactive_users(datab, eng):
     :param eng: engine
     :return: None
     """
-    now = datetime.datetime.now()
-    oneyearago = datetime.date(now.year-1, now.month, now.day)
-    users_actif_last_year = []
+    now = datetime.\
+        datetime.now()
+    time = period_check(now, timeperiod)
+    active_users_for_timeperiod = []
     users = []
-    user, session = datab.galaxy_user, datab.galaxy_session
+    group_association = {}
+    user, session, group_assoc, group = datab.galaxy_user, datab.galaxy_session, datab.user_group_association, \
+                                        datab.galaxy_group
     sele = select([user.email])\
         .where(user.deleted == False)\
         .where(user.id == session.user_id)\
-        .where(session.update_time > oneyearago)\
+        .where(session.update_time > time)\
         .group_by(user.email)
     with eng.connect() as conn:
         result = conn.execute(sele)
         for row in result:
-            users_actif_last_year.append(row[0])
+            active_users_for_timeperiod.append(row[0])
     sele = select([user.email])\
         .where(user.deleted == False)
     with eng.connect() as conn:
         result = conn.execute(sele)
         for row in result:
             users.append(row[0])
-    all_users_inactif = set(users) - set(users_actif_last_year)
-    extern_users_inactif = []
-    for user in all_users_inactif:
-        if '@pasteur.fr' not in user:
-            extern_users_inactif.append(user)
+    sele = select([group.name, user.email])\
+        .where(group.id == group_assoc.group_id)\
+        .where(user.id == group_assoc.user_id)
+    with eng.connect() as conn:
+        result = conn.execute(sele)
+        for row in result:
+            if row[0] in group_association:
+                group_association[row[0]].append(row[1])
+            else:
+                group_association[row[0]] = [row[1]]
+
+    all_inactive_users = set(users) - set(active_users_for_timeperiod)
+    external_inactive_users = []
+    for user in all_inactive_users:
+        if user not in group_association["pasteur_users"]:
+            external_inactive_users.append(user)
     message = "Total users: {0}\n" \
-              "Actives users last year: {1}\n" \
-              "Inactives users last year: {2}\n{3}\n" \
-              "Not Pasteur and inactives users last year {4}\n{5}\n".format(len(users), len(users_actif_last_year),
-                                                                            len(all_users_inactif),
-                                                                            " ".join(all_users_inactif),
-                                                                            len(extern_users_inactif),
-                                                                            " ".join(extern_users_inactif))
-    sendmail(ADMINS_EMAILS, message, "Galaxy Pasteur inactive report")
+              "Actives users in the last {1} month(s):  {7}\n {3} \n" \
+              "Inactives users in the last {1} month(s): {2}\n{4}\n" \
+              "Not Pasteur and inactives users in the last {1} month(s): {5}\n{6}\n".format(len(users), timeperiod,
+                                                                            len(all_inactive_users), " ".join(active_users_for_timeperiod),
+
+                                                                            " ".join(all_inactive_users),
+                                                                            len(external_inactive_users),
+                                                                            " ".join(external_inactive_users), len(active_users_for_timeperiod))
+    sendmail(ADMINS_EMAILS, message, "Galaxy Pasteur inactive users Report")
 
     return
 
@@ -90,6 +120,7 @@ def config_parsing(configfile):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--configfile", help="config file of galaxy")
+    parser.add_argument("--timeperiod", help="time in month to define inactive user")
     args = parser.parse_args()
     database, engine = config_parsing(args.configfile)
-    list_inactive_users(database, engine)
+    list_inactive_users(database, engine, args.timeperiod)
